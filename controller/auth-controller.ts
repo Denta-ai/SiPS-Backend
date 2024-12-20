3c6660af22ec1c3f65a38e0c32b7ea817673bc0d
VincentAlfa <vincent111232@gmail.com>
2024-12-20 18:15:57 +0700

    feat: remove dist and add resetPassport controller

--- controller/auth-controller.ts
+++ controller/auth-controller.ts
@@ -3,4 +3,4 @@ import { Request, Response } from 'express';
 const register = (req: Request, res: Response) => {};
 const login = (req: Request, res: Response) => {};
 const forgotPassword = (req: Request, res: Response) => {};
-
+const resetPassword = (req: Request, res: Response) => {};
